c478072e685d691292de248af712cd435471d4b5
Lu4nScr1pt1ng <rd1qys@gmail.com>
2023-07-11 00:09:52 -0300

    add new plugins and better colorscheme

--- lua/plugins/crucial/treesitter.lua
+++ lua/plugins/crucial/treesitter.lua
@@ -57,46 +57,46 @@ local configs = {
       node_decremental = "grm",
     },
   },
-  -- textobjects = {
-  --   select = {
-  --     enable = true,
-  --     lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-  --     keymaps = {
-  --       -- You can use the capture groups defined in textobjects.scm
-  --       ["aa"] = "@parameter.outer",
-  --       ["ia"] = "@parameter.inner",
-  --       ["af"] = "@function.outer",
-  --       ["if"] = "@function.inner",
-  --       ["ac"] = "@class.outer",
-  --       ["ic"] = "@class.inner",
-  --     },
-  --   },
-  --   move = {
-  --     enable = true,
-  --     set_jumps = true, -- whether to set jumps in the jumplist
-  --     goto_next_start = {
-  --       ["]m"] = "@function.outer",
-  --       ["]]"] = "@class.outer",
-  --     },
-  --     goto_next_end = {
-  --       ["]M"] = "@function.outer",
-  --       ["]["] = "@class.outer",
-  --     },
-  --     goto_previous_start = {
-  --       ["[m"] = "@function.outer",
-  --       ["[["] = "@class.outer",
-  --     },
-  --     goto_previous_end = {
-  --       ["[M"] = "@function.outer",
-  --       ["[]"] = "@class.outer",
-  --     },
-  --   },
-  --   swap = {
-  --     enable = true,
-  --     swap_next = swap_next,
-  --     swap_previous = swap_prev,
-  --   },
-  -- },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ["aa"] = "@parameter.outer",
+        ["ia"] = "@parameter.inner",
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = "@class.outer",
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
+      },
+    },
+    swap = {
+      enable = true,
+      swap_next = swap_next,
+      swap_previous = swap_prev,
+    },
+  },
   matchup = {
     enable = true,
   },
@@ -113,7 +113,8 @@ return {
     "nvim-treesitter/nvim-treesitter",
     dependencies = {
       -- "nvim-treesitter/nvim-treesitter-textobjects",
-      -- "JoosepAlviste/nvim-ts-context-commentstring",
+      "JoosepAlviste/nvim-ts-context-commentstring",
+      "nvim-treesitter/nvim-treesitter-context",
       -- "RRethy/nvim-treesitter-endwise",
       "windwp/nvim-ts-autotag",
     },
